2d3ba19572c49b9214c8b277f1dc0b73b8b3b43d
karanp29 <karanpanchal.git@gmail.com>
2024-05-21 22:59:01 +0530

    dataset-added

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 import streamlit as st
-
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 st.write("# Stock Price Prediction | LSTM")
+
+# Choose a stock
+selection = st.selectbox( "Choose Stock", ['RELIANCE','HDFCBANK','BAJAJFINSV'])
+
+st.write("### "+selection)
+
+df = pd.read_csv("dataset/"+selection+".csv")
+
+# Display the dataframe
+st.write("### DataFrame")
+st.write(df)
+
+# Display df.describe()
+st.write("### Summary Statistics")
+st.write(df.describe())
+
+st.write("### Histogram")
+fig, ax = plt.subplots()
+ax.hist(df['Close'], bins=20)
+st.pyplot(fig)
+
+
